3adc79db3638bb113887fe465df41ee7fc787840
Michael Johann <mjohann@rails-experts.com>
2024-08-02 14:55:47 +0200

    Yabai and sketchybar works now

--- home.nix
+++ home.nix
@@ -102,6 +102,14 @@ in
   home.username = "${user.name}";
   home.homeDirectory = "${user.homeDir}";
 
+  xdg.configFile = {
+    "sketchybar/sketchybarrc" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix/dotfiles/sketchybar/sketchybarrc";
+      onChange = ''
+        chmod +x ${config.xdg.configHome}/sketchybar/sketchybarrc
+      '';
+    };
+  };
 
   home.file = {
     ".stack/config.yaml".text = lib.generators.toYAML { } {
@@ -117,42 +125,54 @@ in
     };
 
     ".yabairc".text = ''
-      	#!/usr/bin/env sh
-        # global settings
-        yabai -m config mouse_follows_focus          off
-        yabai -m config focus_follows_mouse          off
-        yabai -m config window_placement             second_child
-        yabai -m config window_topmost               off
-        yabai -m config window_opacity               off
-        yabai -m config window_opacity_duration      0.0
-        yabai -m config window_shadow                on
-        yabai -m config window_border                on
-        yabai -m config window_border_width          4
+      #!/usr/bin/env sh
+      # global settings
+      yabai -m config mouse_follows_focus          off
+      yabai -m config focus_follows_mouse          off
+      yabai -m config window_placement             second_child
+      yabai -m config window_topmost               off
+      yabai -m config window_opacity               off
+      yabai -m config window_opacity_duration      0.0
+      yabai -m config window_shadow                on
+      yabai -m config window_border                on
+      yabai -m config window_border_width          4
 
-        yabai -m config active_window_opacity        1.0
-        yabai -m config normal_window_opacity        0.90
-        yabai -m config split_ratio                  0.50
-        yabai -m config auto_balance                 off
-        yabai -m config mouse_modifier               fn
-        yabai -m config mouse_action1                move
-        yabai -m config mouse_action2                resize
+      yabai -m config active_window_opacity        1.0
+      yabai -m config normal_window_opacity        0.90
+      yabai -m config split_ratio                  0.50
+      yabai -m config auto_balance                 off
+      yabai -m config mouse_modifier               fn
+      yabai -m config mouse_action1                move
+      yabai -m config mouse_action2                resize
 
-        # general space settings
-        yabai -m config layout                       bsp
-        yabai -m config top_padding                  4
-        yabai -m config bottom_padding               4
-        yabai -m config left_padding                 4
-        yabai -m config right_padding                4
-        yabai -m config window_gap                   4
+      # general space settings
+      yabai -m config layout                       bsp
+      yabai -m config top_padding                  4
+      yabai -m config bottom_padding               4
+      yabai -m config left_padding                 4
+      yabai -m config right_padding                4
+      yabai -m config window_gap                   4
 
-        yabai -m rule --add app="^CopyQ$" manage=off
-        yabai -m rule --add app="^System Settings$" manage=off
-        echo "yabai configuration loaded.."
+      yabai -m rule --add app="^CopyQ$" manage=off
+      yabai -m rule --add app="^System Settings$" manage=off
+
+      yabai -m space 1 --label one
+      yabai -m space 2 --label two
+      yabai -m space 3 --label three
+      yabai -m space 4 --label four
+      yabai -m space 5 --label five
+      yabai -m space 6 --label six
+      yabai -m space 7 --label seven
+      yabai -m space 8 --label eight
+      yabai -m space 9 --label nine
+      yabai -m space 10 --label ten
+
+      yabai -m signal --add event=window_focused action="sketchybar --trigger window_focus"
+      yabai -m config external_bar all:$(sketchybar --query bar | jq .height):0
+
+      echo "yabai configuration loaded.."
     '';
 
-    ".config/sketchybar/sketchybarrc" = {
-      source = ./dotfiles/sketchybar/sketchybarrc;
-    };
   };
 
   home.sessionVariables = {
