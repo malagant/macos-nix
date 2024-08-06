68cc41667567c8c9b86830b6c1e433e5dd4677ee
Michael Johann <mjohann@rails-experts.com>
2024-08-06 10:09:24 +0200

    yabai

--- home.nix
+++ home.nix
@@ -135,35 +135,43 @@ in
 
     ".yabairc".text = ''
       #!/usr/bin/env sh
-      # global settings
-      yabai -m config mouse_follows_focus          off
-      yabai -m config focus_follows_mouse          off
-      yabai -m config window_placement             second_child
-      yabai -m config window_topmost               off
-      yabai -m config window_opacity               off
-      yabai -m config window_opacity_duration      0.0
-      yabai -m config window_shadow                on
-      yabai -m config window_border                on
-      yabai -m config window_border_width          4
+      yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
+      sudo yabai --load-sa
 
-      yabai -m config active_window_opacity        1.0
-      yabai -m config normal_window_opacity        0.90
-      yabai -m config split_ratio                  0.50
-      yabai -m config auto_balance                 off
-      yabai -m config mouse_modifier               fn
-      yabai -m config mouse_action1                move
-      yabai -m config mouse_action2                resize
-
-      # general space settings
-      yabai -m config layout                       bsp
-      yabai -m config top_padding                  4
-      yabai -m config bottom_padding               4
-      yabai -m config left_padding                 4
-      yabai -m config right_padding                4
-      yabai -m config window_gap                   4
+      yabai -m config                                 \
+          external_bar                 off:40:0       \
+          menubar_opacity              1.0            \
+          mouse_follows_focus          off            \
+          focus_follows_mouse          off            \
+          display_arrangement_order    default        \
+          window_origin_display        default        \
+          window_placement             second_child   \
+          window_zoom_persist          on             \
+          window_shadow                on             \
+          window_animation_duration    0.0            \
+          window_animation_easing      ease_out_circ  \
+          window_opacity_duration      0.0            \
+          active_window_opacity        1.0            \
+          normal_window_opacity        0.90           \
+          window_opacity               off            \
+          insert_feedback_color        0xffd75f5f     \
+          split_ratio                  0.50           \
+          split_type                   auto           \
+          auto_balance                 off            \
+          top_padding                   4             \
+          bottom_padding                4             \
+          left_padding                  4             \
+          right_padding                 4             \
+          window_gap                   06             \
+          layout                       bsp            \
+          mouse_modifier               fn             \
+          mouse_action1                move           \
+          mouse_action2                resize         \
+          mouse_drop_action            swap
 
       yabai -m rule --add app="^CopyQ$" manage=off
       yabai -m rule --add app="^System Settings$" manage=off
+      yabai -m rule --add app="^NordPass$" manage=off
 
       yabai -m space 1 --label one
       yabai -m space 2 --label two
