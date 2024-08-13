f1b206a29d0b7e0b5c94428c4384d7f6a838e552
Michael Johann <mjohann@rails-experts.com>
2024-08-13 11:07:55 +0200

    Credential Helper

--- config/git.nix
+++ config/git.nix
@@ -3,8 +3,14 @@
     enable = true;
     userName = "Michael Johann";
     userEmail = "mjohann@rails-experts.com";
+    difftastic.enable = true;
     aliases = {
       st = "status";
+    };
+    extraConfig = {
+      credential = {
+        helper = "store";
+      };
     };
   };
 }
