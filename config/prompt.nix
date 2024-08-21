307077ab3671c55d0b2a684a0d3f2dd422c239a5
Michael Johann <mjohann@rails-experts.com>
2024-08-21 08:21:18 +0200

    k8s prompt

--- config/prompt.nix
+++ config/prompt.nix
@@ -2,15 +2,5 @@
   programs.starship = {
     enable = true;
     # Configuration written to ~/.config/starship.toml
-    settings = {
-      # add_newline = false;
-
-      # character = {
-      #   success_symbol = "[➜](bold green)";
-      #   error_symbol = "[➜](bold red)";
-      # };
-
-      # package.disabled = true;
-    };
   };
 }
--- home.nix
+++ home.nix
@@ -23,6 +23,7 @@ in
     bat
     bruno
     byobu
+    clusterctl
     direnv
     docker
     docker-compose
@@ -87,6 +88,7 @@ in
     k = "kubecolor";
     kgn = "k get nodes";
     kgns = "k get ns";
+    clc = "/usr/local/bin/clusterctl";
   };
 
   # Misc configuration files --------------------------------------------------------------------{{{
@@ -95,23 +97,12 @@ in
   home.homeDirectory = "${user.homeDir}";
 
   xdg.configFile = {
-    # "sketchybar/sketchybarrc" = {
-    #   source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix/dotfiles/sketchybar/sketchybarrc";
-    #   onChange = ''
-    #           chmod +x ${config.xdg.configHome}/sketchybar/sketchybarrc
-    #     			/usr/bin/rsync -av ${config.home.homeDirectory}/.config/nix/dotfiles/sketchybar ${config.xdg.configHome} --exclude "${config.home.homeDirectory}/.config/nix/dotfiles/sketchybar/sketchybarrc"
-    #           /opt/homebrew/bin/sketchybar --reload
-    #   '';
-    # };
     "aerospace/aerospace.toml" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix/dotfiles/aerospace/aerospace.toml";
     };
-    # "skhd/skhdrc" = {
-    #   source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix/dotfiles/skhd/skhdrc";
-    #   onChange = ''
-    #     chmod +x ${config.xdg.configHome}/skhd/skhdrc
-    #   '';
-    # };
+    "starship.toml" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix/dotfiles/starship/starship.toml";
+    };
   };
 
   home.file = {
@@ -126,64 +117,6 @@ in
       };
       nix.enable = true;
     };
-
-    #   ".yabairc".text = ''
-    #     #!/usr/bin/env sh
-    #     yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
-    #     sudo yabai --load-sa
-    #
-    #     yabai -m config                                 \
-    #         external_bar                 off:40:0       \
-    #         menubar_opacity              1.0            \
-    #         mouse_follows_focus          off            \
-    #         focus_follows_mouse          off            \
-    #         display_arrangement_order    default        \
-    #         window_origin_display        default        \
-    #         window_placement             second_child   \
-    #         window_zoom_persist          on             \
-    #         window_shadow                on             \
-    #         window_animation_duration    0.0            \
-    #         window_animation_easing      ease_out_circ  \
-    #         window_opacity_duration      0.0            \
-    #         active_window_opacity        1.0            \
-    #         normal_window_opacity        0.90           \
-    #         window_opacity               off            \
-    #         insert_feedback_color        0xffd75f5f     \
-    #         split_ratio                  0.50           \
-    #         split_type                   auto           \
-    #         auto_balance                 off            \
-    #         top_padding                   4             \
-    #         bottom_padding                4             \
-    #         left_padding                  4             \
-    #         right_padding                 4             \
-    #         window_gap                   06             \
-    #         layout                       bsp            \
-    #         mouse_modifier               fn             \
-    #         mouse_action1                move           \
-    #         mouse_action2                resize         \
-    #         mouse_drop_action            swap
-    #
-    #     yabai -m rule --add app="^CopyQ$" manage=off
-    #     yabai -m rule --add app="^System Settings$" manage=off
-    #     yabai -m rule --add app="^NordPass$" manage=off
-    #
-    #     yabai -m space 1 --label one
-    #     yabai -m space 2 --label two
-    #     yabai -m space 3 --label three
-    #     yabai -m space 4 --label four
-    #     yabai -m space 5 --label five
-    #     yabai -m space 6 --label six
-    #     yabai -m space 7 --label seven
-    #     yabai -m space 8 --label eight
-    #     yabai -m space 9 --label nine
-    #     yabai -m space 10 --label ten
-    #
-    #     yabai -m signal --add event=window_focused action="sketchybar --trigger window_focus"
-    #     yabai -m config external_bar all:$(sketchybar --query bar | jq .height):0
-    #
-    #     echo "yabai configuration loaded.."
-    #   '';
-    #
   };
 
   home.sessionVariables = {
