94e21304a6e4dfe6ecc7c70465d5fa8ec3485866
Michael Johann <mjohann@rails-experts.com>
2024-11-08 12:07:19 +0100

    Linux

--- flake.nix
+++ flake.nix
@@ -1,39 +1,29 @@
 {
-  description = "Darwin configuration for Michael Johann";
+  description = "Linux configuration for Michael Johann";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/aarch64-darwin";
-
-    darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, flake-utils, home-manager, darwin, ... }:
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
     let
-      # utils = flake-utils;
       homeDir = builtins.getEnv "HOME";
       user = import "${homeDir}/.config/nix/user.nix";
-      # user = import ./user.nix;
     in
     {
-      # My `nix-darwin` configs
-      darwinConfigurations = {
-        ${user.hostname} = darwin.lib.darwinSystem {
-          system = builtins.currentSystem;
+      # My `home-manager` configurations for Linux
+      homeConfigurations = {
+        ${user.hostname} = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "x86_64-linux"; # Change this to your system architecture, e.g., "aarch64-linux" if needed
+          };
+          
+          # User-specific configuration module
           modules = [
-            # Main `nix-darwin` config
+            ./home.nix
             ./configuration.nix
-            # `home-manager` module
-            home-manager.darwinModules.home-manager
-            {
-              # `home-manager` config
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.${user.name} = import ./home.nix;
-            }
           ];
         };
       };
